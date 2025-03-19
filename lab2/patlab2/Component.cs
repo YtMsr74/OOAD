76fc946523d73f66bf4ca621d6585335bb4276cb
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2025-03-20 04:03:45 +0700

    Add files via upload

---
+++ lab2/patlab2/Component.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patlab2
+{
+    internal abstract class Component
+    {
+        public string Name;
+        public abstract long GetSize();
+    }
+}
---
+++ lab2/patlab2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace patlab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            string path = edPath.Text;
+            if (Directory.Exists(path))
+            {
+                labelError.Visible = false;
+                string line = "";
+                gridResult.Rows.Clear();
+                CompFolder root = new CompFolder(path);
+                ShowFiles(root,line);
+            }
+            else
+            {
+                labelError.Visible = true;
+            }
+        }
+
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                edPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void ShowFiles(Component c, string line)
+        {
+            long size = c.GetSize() / 1024 + 1;
+            gridResult.Rows.Add(line + c.Name, size.ToString() + " KB");
+            if (c is CompFolder)
+            {
+                CompFolder folder = (CompFolder)c;
+                foreach (Component child in folder.GetChildren())
+                {
+                    ShowFiles(child, line + '-');
+                }
+            }
+        }
+    }
+}
