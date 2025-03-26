70fddc8cfc5e2f1fab4594ebedd8aea0a5c16169
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2025-03-27 02:45:41 +0700

    Add files via upload

---
+++ lab2/patlab2/patlab2/Component.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patlab2
+{
+    internal abstract class Component
+    {
+        public string Name;
+        public string Path;
+        public abstract long GetSize();
+    }
+}
---
+++ lab2/patlab2/patlab2/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace patlab2
+{
+    public partial class Form1 : Form
+    {
+        string curPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            Analyze();
+        }
+
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                edPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void ShowFiles(Component c, string line)
+        {
+            long size = c.GetSize() / 1024 + 1;
+            gridResult.Rows.Add(line + c.Name, size.ToString() + " KB", c);
+
+            if (c is CompFolder)
+            {
+                CompFolder folder = (CompFolder)c;
+                foreach (Component child in folder.GetChildren())
+                {
+                    ShowFiles(child, line + '-');
+                }
+            }
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            string filename = edFileName.Text;
+            if (gridResult.CurrentRow != null && gridResult.CurrentRow.Cells[2].Value is CompFolder)
+            {
+                CompFolder folder = gridResult.CurrentRow.Cells[2].Value as CompFolder;
+                string path = folder.Path + "\\" + filename;
+                try
+                {
+                    File.Create(path);
+                    folder.Add(new CompFile(path));
+                    Analyze();
+                    labelFile.Visible = false;
+                }
+                catch(Exception er)
+                {
+                    labelFile.Text = "Ошибка: " + er.Message;
+                    labelFile.Visible = true;
+                }
+            }
+            else
+            {
+                labelFile.Text = "Ошибка: Папка не выбрана";
+                labelFile.Visible = true;
+            }
+        }
+
+        private void Analyze()
+        {
+            string path = edPath.Text;
+            if (Directory.Exists(path))
+            {
+                labelError.Visible = false;
+                string line = "";
+                gridResult.Rows.Clear();
+                CompFolder root = new CompFolder(path);
+                ShowFiles(root, line);
+                curPath = path;
+            }
+            else
+            {
+                labelError.Visible = true;
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            labelFile.Visible = false;
+        }
+    }
+}
