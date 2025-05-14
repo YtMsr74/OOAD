a3bc46de86e43121396c601391b18dd6c05e131e
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2025-05-14 22:35:27 +0700

    Add files via upload

---
+++ lab4/nopat/patlab4/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patlab4
+{
+    internal class Vehicle
+    {
+        public int id;
+        public string name;
+        public int year;
+        public string country;
+        public string engine;
+        public string layout;
+        public string weight;
+        public Image image; 
+
+        public Vehicle(int id, string name, int year, string country, string engine, string layout, string weight, string path)
+        {
+            this.id = id;
+            this.name = name;
+            this.year = year;
+            this.country = country;
+            this.engine = engine;
+            this.layout = layout;
+            this.weight = weight;
+            this.image = Image.FromFile(path);
+        }
+    }
+}
